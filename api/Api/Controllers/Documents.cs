29e4a2be75133729c82d86a5129414a893ac40e1
Sebastian <sebastian@elstadt.com>
2026-05-14 18:03:51 -0700

    Added document controller endpoint to search vector store

--- api/Api/Controllers/Documents.cs
+++ api/Api/Controllers/Documents.cs
@@ -14,4 +14,15 @@ public class DocumentsController(IDocumentPipeline documentPipeline) : Controlle
         await documentPipeline.IngestAsync(strm, file.FileName, HttpContext.RequestAborted);
         return Ok();
     }
+
+    [HttpGet]
+    public async Task<IActionResult> SearchDocumentsAsync(
+        [FromQuery(Name = "s")] string searchQuery,
+        [FromQuery(Name = "top")] int topK,
+        [FromQuery(Name = "docId")] Guid? documentId = null
+    )
+    {
+        var hits = await documentPipeline.SearchAsync(searchQuery, topK, documentId, HttpContext.RequestAborted);
+        return Ok(hits);
+    }
 }
